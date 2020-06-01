7b3630581c4736071ce069b487eff7fe92ef083a
Caio Souza <caio326@icloud.com>
2020-06-01 18:58:26 -0300

    :sparkles: Add early version of black interface

--- javascripts/sketch.js
+++ javascripts/sketch.js
@@ -1,7 +1,18 @@
 function setup() {
-  createCanvas(400, 400);
+  var width = document.getElementById("canvas").offsetWidth;
+  var height = document.getElementById("canvas").offsetHeight;
+  var customCanvas = createCanvas(width, height);
+  customCanvas.parent("canvas");
+  windowResized();
 }
 
 function draw() {
-  ellipse(100,100, 100, 50);
+  line(100,100, 200,200);
+  stroke("#c1c1c1");
+}
+
+function windowResized() {
+  var width = document.getElementById("canvas").offsetWidth;
+  var height = document.getElementById("canvas").offsetHeight;
+  resizeCanvas(width, height);
 }
